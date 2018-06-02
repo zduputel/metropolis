a3d7c7e592ccbf89a8f00b30dcee90480cc6fc65
Zacharie Duputel <zacharie.duputel@unistra.fr>
2018-06-02 17:18:24 +0200

    Merging with distant repo

--- metropolis.py
+++ metropolis.py
@@ -5,7 +5,7 @@ A simple metropolis sampler class
 import numpy as np
 np.random.seed(0)
 
-def metropolis(n_samples,fun_calcLLK,fun_verify,data,m_ini,prior_bounds,prop_cov,verbose=False):
+def metropolis(n_samples,fun_calcLLK,fun_verify,data,m_ini,prior_bounds,prop_cov,LLK_data=False,verbose=False):
 
     ''' 
     Metropolis algorithm 
@@ -17,13 +17,20 @@ def metropolis(n_samples,fun_calcLLK,fun_verify,data,m_ini,prior_bounds,prop_cov
         * m_ini: Initial model
         * prior_bounds: Parameter bounds (uniform priors)
         * prop_cov: Covariance of the (gaussian) proposal PDF
+        * LLK_data: Use False (default) if the calcLLK function only returns log-likelihood values
+                    Use True if the calcLLK function returns log-LLK + one extra parameter (returned at the end of metropolis)
+        * verbose: (default: False) verbose mode
     '''
 
     # Initiate model chain
     M   = np.zeros((n_samples,m_ini.size))
     LLK = np.zeros((n_samples,))
     M[0,:] = m_ini # Initialize the chain
-    LLK[0] = fun_calcLLK(m_ini,data) 
+    if LLK_data:
+        LLK[0],LLK_p = fun_calcLLK(m_ini,data)
+        LLK_d = [LLK_p]
+    else:
+        LLK[0] = fun_calcLLK(m_ini,data) 
 
     # Proposal mean
     prop_mean = np.zeros(m_ini.shape)
@@ -48,7 +55,10 @@ def metropolis(n_samples,fun_calcLLK,fun_verify,data,m_ini,prior_bounds,prop_cov
             continue
 
         # LLK of new model        
-        logLLKn = fun_calcLLK(Mnew, data)
+        if LLK_data:
+            logLLKn,LLK_p = fun_calcLLK(Mnew, data)
+        else:
+            logLLKn = fun_calcLLK(Mnew, data)        
         dLLK    = logLLKn - LLK[i-1]
         
         # Metropolis acceptance/rejection
@@ -63,9 +73,16 @@ def metropolis(n_samples,fun_calcLLK,fun_verify,data,m_ini,prior_bounds,prop_cov
         if accept:
             M[i,:]  = Mnew.copy()
             LLK[i]  = logLLKn
+            if LLK_data:
+                LLK_d.append(LLK_p)
             count += 1
         else:
             M[i,:] = M[i-1,:].copy()
             LLK[i] = LLK[i-1]
+            if LLK_data:            
+                LLK_d.append(LLK_d[-1])
 
-    return M,LLK,count
+    if LLK_data:
+        return M,LLK,LLK_d,count
+    else:
+        return M,LLK,count
